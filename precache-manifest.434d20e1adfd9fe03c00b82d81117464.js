c4b4ab5bcf9cfe2c243af6b0e2247ea0469d16cd
Code Sandbox <hello@codesandbox.io>
2022-03-20 20:13:12 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.434d20e1adfd9fe03c00b82d81117464.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82d747b68d270262ef5e0753faea8e4b",
+    "url": "https://dancaires.github.io/csb-w78h5k/index.html"
+  },
+  {
+    "revision": "500670681d6fbae790f8",
+    "url": "https://dancaires.github.io/csb-w78h5k/static/js/2.88c53422.chunk.js"
+  },
+  {
+    "revision": "0c111bb47635c4033a9a",
+    "url": "https://dancaires.github.io/csb-w78h5k/static/js/main.f6508a9f.chunk.js"
+  },
+  {
+    "revision": "cba53289c4551d9d4aa0",
+    "url": "https://dancaires.github.io/csb-w78h5k/static/js/runtime~main.bf381811.js"
+  }
+]);
